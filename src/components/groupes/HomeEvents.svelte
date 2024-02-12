5333785c0bb5959f86c18a9c6a0809757e0cee28
Tom Chauvel <tom.chauvel@proton.me>
2024-02-12 23:58:59 +0100

    initial commit

---
+++ src/components/groupes/HomeEvents.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import Button from "../individuels/Button.svelte";
+    import LargeCard from "../individuels/LargeCard.svelte";
+</script>
+
+<div class="main">
+
+    <h2>Les events</h2>
+
+    <div class="content">
+
+        <LargeCard eventName="Cave à Flo" date="25/09"></LargeCard>
+        <LargeCard eventName="Cave à Flo" date="25/09"></LargeCard>
+        
+    </div>
+    
+    <div class="action">
+        <Button>Voir plus</Button>
+    </div>
+
+</div>
+
+<style>
+
+    .main {
+        display: flex;
+        flex-direction: column;
+        gap:15px;
+    }
+
+    .content {
+        display: grid;
+        grid-template-columns: repeat(1,1fr);
+        gap:10px;
+        place-items: center;
+    }
+
+    .action {
+        display: grid;
+        place-items: center;
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    import Button from "../components/individuels/Button.svelte";
+
+
+    import HomeEvents from "../components/groupes/HomeEvents.svelte";
+    import HomeEquipe from "../components/groupes/HomeEquipe.svelte";
+    import HomeSalles from "../components/groupes/HomeSalles.svelte";
+    import HomeActus from "../components/groupes/HomeActus.svelte";
+
+
+</script>
+
+<div class="container">
+
+    <HomeEvents></HomeEvents>
+    
+    
+    <HomeSalles></HomeSalles>
+
+    <HomeActus></HomeActus>
+
+    <HomeEquipe></HomeEquipe>
+    
+</div>
+
+<style>
+    .container {
+        display:grid;
+        width:clamp(0px,100%,700px);
+        margin: auto;
+        grid-template-columns: repeat(2, 1fr);
+        gap:40px;
+    }
+
+    .container > :global(:nth-child(1)) { grid-area: 1 / 1 / 2 / 2; }
+    .container > :global(:nth-child(2)) { grid-area: 1 / 2 / 2 / 3; }
+    .container > :global(:nth-child(3)) { grid-area: 2 / 1 / 3 / 3; }
+    .container > :global(:nth-child(4)) { grid-area: 3 / 1 / 4 / 3; }
+
+    @media (max-width : 720px) {
+        .container > :global(:nth-child(1)) { grid-area: 1 / 1 / 2 / 3; }
+        .container > :global(:nth-child(2)) { grid-area: 2 / 1 / 3 / 3; }
+        .container > :global(:nth-child(3)) { grid-area: 3 / 1 / 4 / 3; }
+        .container > :global(:nth-child(4)) { grid-area: 4 / 1 / 5 / 3; }
+    }
+</style>
