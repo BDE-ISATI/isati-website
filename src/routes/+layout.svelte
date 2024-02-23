5dd131d48e7c8bd626b57191e16f30181a57aead
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-02-23 20:19:11 +0100

    reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script>
-	import Header from "$lib/components/individuels/Header.svelte"
-	import { fade  } from 'svelte/transition';
-	
-	export let data
+
 
 </script>
 
@@ -14,33 +11,16 @@
 
 <div class="app">
 
-	<Header></Header>
 	
 	{#key data.pathname}
 		<div id="content" in:fade={{ duration: 1000}}>
 			<slot></slot>
 		</div>
 	{/key}
-	
-	<!-- <Footer></Footer> -->
+
 	
 </div>
 
 <style>
-	div.app{
-		display: flex;
-		flex-direction: column;
-		gap:1em;
-	}
-
-	#content {
-		overflow:auto;
-		display: block;
-		position: relative;
-		padding: 16px;
-		margin:0;
-		width:calc( 100% - 32px);
-		overflow: overlay;
-	}
 
 </style>
