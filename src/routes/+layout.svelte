b24387df12904b829dd4099b6df80a3d071b73fa
Tom Chauvel <tom.chauvel@proton.me>
2024-03-09 14:14:09 +0000

    texte pour suivre le chargement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,18 +12,21 @@
     import Footer from "$lib/components/layout/Footer.svelte";
 	
 	let retour = ""
-	let loaded = false
+	let loaded = 0
 
 	onMount(async() =>{
 		$events = await (await fetch(apiUri + "events")).json()
+		loaded += 1
 		$members = await (await fetch(apiUri + "members")).json()
+		loaded += 1
 		$salles = await (await fetch(apiUri + "salles/events")).json()
+		loaded += 1
 		$articles = await (await fetch(apiUri + "articles")).json()
+		loaded += 1
 
 		let temp = window.location.pathname.split("/")
 		temp.splice(-1)
 		retour = window.location.origin +  "/" + temp.join("/")
-		loaded = true
 	})
 
 
@@ -59,13 +62,16 @@
 	<meta name="description" content="Site internet de L'Isati, BDE de l'ESIR.">
 </svelte:head>
 
-{#if !loaded}
+{#if loaded < 4}
 	<div id="loader" out:fade={{ duration: 1000}}>
-		<img width="90" height="90" src="/isati.svg" style="position:absolute"/>
-		<svg width="24" height="24" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg">
-			<path opacity="0.2" fill-rule="evenodd" clip-rule="evenodd" d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="currentColor"/>
-			<path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="currentColor" />
-		</svg>
+		<div class="logoloading">
+			<img width="90" height="90" src="/isati.svg" style="position:absolute"/>
+			<svg class="spinner" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+				<path opacity="0.2" fill-rule="evenodd" clip-rule="evenodd" d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="currentColor"/>
+				<path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="currentColor" />
+			</svg>
+		</div>
+		<span>{loaded/4*100} %</span>
 	</div>
 {:else}
 	<div class="app">
@@ -91,8 +97,10 @@
 <style scoped>
 	#loader {
 		transition: opacity 1s;
-		display:grid;
-		place-items: center;
+		display:flex;
+		justify-content:center;
+		flex-direction: column;
+		text-align: center;
 		position: absolute;
 		z-index: 50000;
 		height: 100%;
@@ -100,13 +108,18 @@
 		background-color: var(--primary);
 	}
 
-	#loader > svg{
+	.logoloading {
+		display:grid;
+		place-items: center;
+	}
+
+	#loader svg.spinner{
 		animation-name: turn;
 		animation-duration: 1s;
 		animation-iteration-count: infinite;
 		animation-timing-function: linear;
-		width:150px;
 		height:150px;
+		width: 150px;
 		color:var(--white)
 	}
 
