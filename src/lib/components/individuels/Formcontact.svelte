e154653d9ade177e6fdc961c992ea3b5161a1ce9
Tom Chauvel <tom.chauvel@proton.me>
2024-04-10 21:00:01 +0200

    formulaire

---
+++ src/lib/components/individuels/Formcontact.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import Button from "./Button.svelte";
+
+    let nom = ""
+    let contact = ""
+    let msg = ""
+    let url = "https://discord.com/api/webhooks/1227688957962354779/lBbZ01Y6tE-_SfjIpa8rfzZy1b5KwZsEY_PR8fcYgNMOu35r3PpG7iOcaBZLWQOui8Ro"
+
+    async function send(){
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({content:`# Destinataire : ${nom}\n## Contact : ${contact}\n\n${msg}`}),
+        })
+        if (response.status == 204) {
+            alert("Message Envoyé, merci 🙂")
+        }
+        else {
+            alert("Message Non Envoyé, désolé 😥")
+        }
+    }
+</script>
+
+<div>
+    <label>Votre nom</label>
+    <input bind:value={nom}>
+    <label>Vous recontacter</label>
+    <input bind:value={contact} >
+    <label>Votre message</label>
+    <textarea bind:value={msg} rows="10"></textarea>
+    <Button on:click={send}>Envoyer</Button>
+</div>
+
+<style>
+    
+    div {
+        display: flex;
+        flex-direction: column;
+        gap:8px;
+        width: 300px;;
+    }
+    textarea{
+        flex:1;
+        
+        resize: none;
+        outline: unset;
+        border: unset;
+        color : var(--text);
+    }
+</style>
--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { siInstagram, siDiscord, siFacebook, siGithub, siYoutube, siLinkedin } from "simple-icons";
 
+    import Formcontact from "$lib/components/individuels/Formcontact.svelte"
+
     let icons = [
         {url:"https://www.instagram.com/isatibde/",icon:siInstagram},
         {url:"https://discord.gg/jfpVdAZT",icon:siDiscord},
@@ -9,23 +11,35 @@
         {url:"https://www.youtube.com/@isatibde",icon:siYoutube},
         {url:"https://www.linkedin.com/company/bde-isati/",icon:siLinkedin},
     ]
-
+    
 </script>
+
 <footer>
     <a href="/tools">Nos outils</a>
     <a href="https://forms.gle/tPL5Txmq9Qb5t2Pn6" target="_blank">Nous soumettre votre article</a>
+    
+    <hr>
+
     <div class="networks">
         {#each icons as icon}
-            <a href={icon.url} target="_blank" style={`color:#${icon.icon.hex};`}>
-                {@html icon.icon.svg}
-            </a>
+        <a href={icon.url} target="_blank" style={`color:#${icon.icon.hex};`}>
+            {@html icon.icon.svg}
+        </a>
         {/each}
     </div>
+    <hr>
 
+    <h4>Nous contacter</h4>
+    
+    <Formcontact></Formcontact>
+    
+    <hr>
+    
     <h4>Technologies utilisées</h4>
     <span>Hébergé sur AWS</span>
     <span>Svelte et editorjs </span>
-
+    
+    <hr>
     
     <h4>Avec le soutien</h4>
     <div class="univs">
@@ -33,10 +47,19 @@
         <img src="/logo/UR1.svg">
     </div>
     
+    <hr>
+    
     <h4>Isati © {(new Date()).getFullYear()}</h4>
 </footer>
 
 <style>
+
+
+hr {
+    width: 100%;
+    opacity: 10%;
+    margin : 16px;
+}
 
 .univs{
     display: grid;
@@ -57,7 +80,7 @@ footer {
     padding:48px 16px;
     display: flex;
     flex-direction: column;
-    gap:32px;
+    /* gap:32px; */
     align-items: center;
 }
 
