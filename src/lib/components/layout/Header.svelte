4fe3e9296a6b72338d57e0bca91268f7f2e342ff
Tom Chauvel <tom.chauvel@proton.me>
2024-04-13 17:17:09 +0200

    reduction svg size

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -5,8 +5,6 @@
 <div class="justify-center p-3 flex md:hidden">
 	<a class="z-10 text-[var(--white)]" href="/"><Logo></Logo></a>
 
-	<svg class="h-24 w-full absolute top-0 left-0 text-primary dark:text-container-800" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 1255 303" fill="currentColor">
-		<path d="M0 0H1255V230.5C1255 230.5 1093 302.5 627.5 302.5C162 302.5 0 230.5 0 230.5V0Z" fill="currentColor"/>
-	</svg>
+	<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-24 w-full absolute top-0 left-0 text-primary dark:text-container-800" preserveAspectRatio="none" viewBox="0 0 1255 303"><path d="M0 0h1255v231s-162 72-627 72C162 303 0 231 0 231V0Z"/></svg>
 		
 </div>
