e7a274107081ff525840c2ff1df728b343fc0dd2
Tom Chauvel <tom.chauvel@proton.me>
2024-04-13 15:41:52 +0200

    fix calendrier

--- src/routes/events/calendar.svelte
+++ src/routes/events/calendar.svelte
@@ -20,50 +20,23 @@
     } while(firstDay<=lastDay)
 </script>
 
-<div class="grid gridCal bg-container-700 ring-1 ring-container-700 gap-[1.1px] rounded-lg overflow-hidden">
-		{#each ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"] as day}
-			<div class="text-center p-2 bg-container-900">
-				<span>{day}</span>
-			</div>
-		{/each}
-		{#each days as day}
-			<div class="p-2 bg-container-900">
-				<time class="leading-6 bg-primary size-5 grid place-items-center text-white font-bold rounded-full text-xs">{day.getDate()}</time>
-			
-				<ol class="content">
-					{#each $events as event}
-						{#if day.getDate() == new Date(event.date).getDate() && day.getMonth() == new Date(event.date).getMonth()}
-							<a class="eventLink" href={`/article?id=${event.article}`}>{event.nom}</a>	
-						{/if}
-					{/each}
-                </ol>
-			</div>
-		{/each}
-
-	</div>
-
-    <style>
-
-        .gridCal{
-            grid-template-columns: repeat(7,1fr);
-            grid-template-rows: auto  repeat(4,100px);
-        }
-
-    
-        .eventLink{
-            background: var(--secondary);
-            padding:4px;
-            border-radius: 16px;
-            width: calc(100% - 8px );
-            overflow: hidden;
-            display: block;
-        }
-    
-
-        @media (max-width : 767px) {
-            .content {
-                grid-template-columns: repeat(1,1fr);
-            }
-        }
-    
-    </style>
+<div class="grid gridCal bg-container-700 ring-1 ring-container-700 gap-[1.1px] rounded-lg overflow-auto grid-cols-[repeat(7,calc(729.6px_/_7))] grid-rows-[auto_repeat(4,8em)]">
+    {#each ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"] as day}
+        <div class="text-center p-2 bg-container-900 w-30">
+            <span>{day}</span>
+        </div>
+    {/each}
+    {#each days as day}
+        <div class="p-2 bg-container-900 height-30">
+            <time class="leading-6 bg-primary size-5 grid place-items-center text-white font-bold rounded-full text-xs">{day.getDate()}</time>
+        
+            <ol class="flex gap-1 flex-col mt-1">
+                {#each $events as event}
+                    {#if day.getDate() == new Date(event.date).getDate() && day.getMonth() == new Date(event.date).getMonth()}
+                        <a class="text-sm text-ellipsis	overflow-hidden bg-container-800 px-1 py-0.5 rounded-lg w-full leading-0" href={event.article ? `/article?id=${event.article}` : undefined}>{event.nom}</a>	
+                    {/if}
+                {/each}
+            </ol>
+        </div>
+    {/each}
+</div>
