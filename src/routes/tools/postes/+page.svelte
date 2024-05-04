622b54b3fe7612d4d31534eae8a3bbdcda37a32c
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-05-04 22:14:57 +0200

    fix fonts

--- src/routes/tools/postes/+page.svelte
+++ src/routes/tools/postes/+page.svelte
@@ -5,11 +5,23 @@
     import PhotoSemaine from "./components/PhotoSemaine.svelte";
     import Fin from "./components/Fin.svelte";
 
+
+    (new FontFace('AzoSans-Bold', 'url(/fonts/AzoSans/AzoSans-Bold.woff2)')).load().then((font) => {
+        document.fonts.add(font);
+    });
+    (new FontFace('AzoSansBlack', 'url(/fonts/AzoSans/AzoSans-Black.woff2)')).load().then((font) => {
+        document.fonts.add(font);
+    });
+    (new FontFace('AzoSansUber', 'url(/fonts/AzoSans/AzoSans-Uber.woff2)')).load().then((font) => {
+        document.fonts.add(font);
+    });
+
 </script>
 <div class="flex gap-6 flex-col">
     <Garde></Garde>
     <Agenda></Agenda>
     <Events></Events>
     <PhotoSemaine></PhotoSemaine>
-    <Fin></Fin>    
+    <Fin></Fin>
+    <hr>
 </div>
