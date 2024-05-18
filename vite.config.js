4fc914227a0631bbcd86017e0839100f86a55c68
Tom Chauvel <tom.chauvel@proton.me>
2024-05-18 17:42:55 +0200

    js compress

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	
+	build:{
+		target: "esnext",
+		minify:"terser",
+		terserOptions: {
+			compress: true,
+			mangle: true,
+
+		}
+	},
+	
 });
