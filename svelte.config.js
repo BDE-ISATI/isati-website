c09755cb0a6d3d533b0e495f5d2e35527ceb2ccf
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-05-18 18:50:35 +0200

    precompress gzip & brotli

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({ postcss: true }),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			precompress:true
+		})
 	}
 };
 
