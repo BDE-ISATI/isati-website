57297de4965d8737c1d79fd84228d888a98690c2
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-06-17 14:49:24 +0200

    birthday ok

--- src/routes/tools/birthday/+page.svelte
+++ src/routes/tools/birthday/+page.svelte
@@ -30,12 +30,16 @@
     afterUpdate(() =>{
         config = {
             backgroundURL:"./anniversaire/fond.png",
-            height:1920,
-            width:1080,
+            height:2944,
+            width:1394,
             canvas:canvas!
         }
+        
         template = new Template(config)
     })
+
+    let cx = 1.12
+    let cy = 1.515
 
     beforeUpdate(async () =>{
         if (!template) return
@@ -45,26 +49,26 @@
         if (files1) {
             // load une image 
             let image = await template.loadImageFile(files1[0])
-            template.rotatedAction( 7, () => template.drawResizeCropImage(image, 190,470,770,770) )
+            template.rotatedAction( 7, () => template.drawResizeCropImage(image, 270,715,1095,1170) )
         }
 
         if (files2) {
             // load une image 
             let image = await template.loadImageFile(files2[0])
-            template.rotatedAction( -11, () => template.drawResizeCropImage(image, 370,1380,390,390) )
+            template.rotatedAction( -11, () => template.drawResizeCropImage(image, 420,2080,500,590) )
         }
 
         await template.filteredAction( `hue-rotate(${huerotate}deg)`, async () => {await template.drawBackground()})
         
-        template.rotatedAction( 7, () => template.drawTexte(nom,550,1320,'Nanami',80,"1","#000000",0,"center") )
-        template.rotatedAction( -10, () => template.drawTexte(rôle,600,1800,'Nanami',40,"1","#dc2323",0,"center") )
+        template.rotatedAction( 7, () => template.drawTexte(nom,780,2005,'Nanami',115,"1","#000000",0,"center") )
+        await template.filteredAction( `hue-rotate(${huerotate}deg)`, () => template.rotatedAction( -11, () => template.drawTexte(rôle,670,2730,'Nanami',45,"1","#dc2323",0,"center") ) )
     })
 
 </script>
 
 <div class="grid gap-4 grid-cols-1" spellcheck="false">
 
-    <canvas class="w-full h-auto" bind:this={canvas}></canvas>
+    <canvas class="w-auto max-h-96 place-self-center" bind:this={canvas}></canvas>
 
     <Input placeholder="Nom" bind:value={nom}/>
     <Input placeholder="Rôle" bind:value={rôle}/>
@@ -72,7 +76,7 @@
     <Input placeholder="Image 1" type="file" bind:files={files1}/>
     <Input placeholder="Image 2" type="file" bind:files={files2}/>
 
-    <Input placeholder="Hue Rotate (-180 à 180)" type="number" bind:value={huerotate}/>
+    <Input placeholder="Hue Rotate" type="range" min={0} max={360} bind:value={huerotate}/>
 
     <Button on:click={() => template.download()}>Télécharger</Button>
 </div>
