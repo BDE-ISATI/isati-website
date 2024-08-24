e64190b10474a96e601097848bbb373a3e293652
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-08-24 14:29:40 +0200

    changes

--- src/lib/components/individuels/Dropdown.svelte
+++ src/lib/components/individuels/Dropdown.svelte
@@ -33,7 +33,7 @@
 
 <div class="relative inline-block text-left w-30">
     <div class="w-full">
-      <button on:click={() => opened=!opened} use:clickOutside={() => {opened=false}}  type="button" class="text-container-100 flex w-full justify-center items-center gap-x-1.5 rounded-md bg-container-700 px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-black ring-opacity-5" id="menu-button" aria-expanded="true" aria-haspopup="true">
+      <button on:click={() => opened=!opened} use:clickOutside={() => {opened=false}}  type="button" class="text-container-100 flex w-full justify-center items-center gap-x-1.5 rounded-xl bg-container-700 px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-black ring-opacity-5" id="menu-button" aria-expanded="true" aria-haspopup="true">
         <svelte:component this={icon} class="text-2xl" weight="fill"/>
         {title}
         <CaretDown/>
@@ -52,7 +52,7 @@
         To: "transform opacity-0 scale-95"
     -->
     {#if opened}
-    <div transition:scale={{ delay: 0, duration: 300 }} class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-container-700 shadow-md ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div transition:scale={{ delay: 0, duration: 300 }} class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-xl bg-container-700 shadow-md ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
       <div class="py-1">
         {#each options as option}
           <button on:click={() => {opened=false;option.action()}} class="text-container-100 block px-4 py-2 text-sm" tabindex="-1">{option.libelle}</button>
