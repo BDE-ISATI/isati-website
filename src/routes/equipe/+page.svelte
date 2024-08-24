889cf1b8cf3a7df4f05b2bfb4d4b3061bde69085
Tom Chauvel <tom.chauvel@proton.me>
2024-08-24 13:59:13 +0200

    drawer

--- src/routes/equipe/+page.svelte
+++ src/routes/equipe/+page.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
 	import Button from "$lib/components/individuels/Button.svelte";
+    import Mansory from "$lib/components/individuels/Mansory.svelte";
 	import NewCard from "$lib/components/individuels/NewCard.svelte";
     import NewCardLine from "$lib/components/individuels/NewCardLine.svelte";
 	import { members_public } from "$lib/store";
 
 </script>
 
-<div class="flex relative gap-4 flex-col">
+<h1 class="uppercase text-3xl font-bold">L'équipe</h1>
 
-	<h1 class="uppercase text-3xl font-bold">L'équipe</h1>
-
-	<div class="columns-xs *:my-4 text-center w-full">
-		{#each Object.keys($members_public) as grp}
-			<NewCard title={grp}>
-				{#each $members_public[grp] as user}
-					<NewCardLine primary={user.nom} secondary={user.rôle} href={user.contact} image={user.photo}></NewCardLine>
-					<hr class="m-2 w-2/3 self-center border-container-500 last:hidden">	
-				{/each}
-			</NewCard>
-		{/each}
-	</div>
-
-</div>
+<Mansory>
+	{#each Object.keys($members_public) as grp}
+		<NewCard title={grp}>
+			{#each $members_public[grp] as user}
+				<NewCardLine primary={user.nom} secondary={user.rôle} href={user.contact} image={user.photo}></NewCardLine>
+				<hr class="m-2 w-2/3 self-center border-container-500 last:hidden">	
+			{/each}
+		</NewCard>
+	{/each}
+</Mansory>
--- src/routes/salles/+page.svelte
+++ src/routes/salles/+page.svelte
@@ -7,6 +7,11 @@
 	import IconeSalle from "$lib/components/logo/IconeSalle.svelte"
 	import IconeAmphi from "$lib/components/logo/IconeAmphi.svelte"
 	import { salles } from "$lib/store";
+    import Mansory from "$lib/components/individuels/Mansory.svelte";
+    import Drawer from "$lib/components/individuels/Drawer.svelte";
+    import { draw } from "svelte/transition";
+    import ButtonIcon from "$lib/components/individuels/ButtonIcon.svelte";
+    import { Gear } from "phosphor-svelte";
 
     function stringify_date(time:number){
         if (time == undefined) return 'updating';
@@ -64,26 +69,40 @@
 		salles_m = change_format((await (await fetch(apiUri + "salles/events" + request)).json())["data"])
 	}
 
+	let show_drawer_val = false
+
+	function show_drawer() {
+		show_drawer_val=true
+	}
+
+	function hide_drawer() {
+		show_drawer_val=false
+	}
+
+	let hidden = true
+	
 </script>
 
-<div class="flex relative gap-4 flex-col items-center">
 
-	<h1 class="text-3xl font-bold">LES SALLES</h1>
-	
-	<div class="flex gap-2">
-		<Input placeholder="Date" bind:value={sDate} type="date" />
-		<Input placeholder="Heure" bind:value={sTime} type="time" />
-		<Button on:click={actualize}>Change</Button>
-	</div>
+<h1 class="text-3xl font-bold">LES SALLES</h1>
 
-	<div class="columns-xs *:my-4 text-center w-full">
-		{#each Object.keys(salles_m) as key}
-			<NewCard title={`Bâtiment ${key}`}>
-				{#each salles_m[key] as salle}
+<Drawer title="Options" hidden={hidden} hide={()=>{hidden=true}}> 
+	<Input placeholder="Date" bind:value={sDate} type="date" />
+	<Input placeholder="Heure" bind:value={sTime} type="time" />
+	<Button on:click={actualize}>Change</Button>
+</Drawer>
+
+<Mansory>
+	{#each Object.keys(salles_m) as key}
+		<NewCard title={`Bâtiment ${key}`}>
+			{#each salles_m[key] as salle}
 				<NewCardLine icone={salle.type=="salle" ? IconeSalle : IconeAmphi} iconeBgClass={salle.state=="Libre" ? "bg-[#2BD02B] rounded-xl" : "bg-primary rounded-xl"} primary={salle.salleID} secondary={salle.state} tertiary={`Jusqu'au ${stringify_date(salle.until)}`}></NewCardLine>
 				<hr class="m-2 w-2/3 self-center border-container-500 last:hidden">	
-				{/each}
-			</NewCard>
-		{/each}
-	</div>
+			{/each}
+		</NewCard>
+	{/each}
+</Mansory>
+
+<div class="fixed bottom-8 right-8">
+	<ButtonIcon bgColor="bg-primary" on:click={() => {hidden=false}}><Gear></Gear></ButtonIcon>
 </div>
