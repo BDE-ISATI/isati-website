29eb76cd6f80679814a4b83eb759acb0d407e483
Tom Chauvel <tom.chauvel@proton.me>
2024-09-12 15:03:53 +0200

    ajout des rôles pour les pings

--- src/lib/components/individuels/FormContact.svelte
+++ src/lib/components/individuels/FormContact.svelte
@@ -5,16 +5,23 @@
 
     let nom = ""
     let contact = ""
+    let pôle = "@everyone"
     let msg = ""
     let url = "https://discord.com/api/webhooks/1227688957962354779/lBbZ01Y6tE-_SfjIpa8rfzZy1b5KwZsEY_PR8fcYgNMOu35r3PpG7iOcaBZLWQOui8Ro"
 
     async function send(){
+        if (nom === "" || contact === "" || msg === ""){
+            alert("Message non Envoyé, merci de remplir tous les champs")
+            return
+        }
+
+
         const response = await fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({content:`# Destinateur : ${nom}\n## Contact : ${contact}\n\n${msg}`}),
+            body: JSON.stringify({content:`# Destinateur : ${nom}\n# Destinataire : ${pôle}\n## Contact : ${contact}\n\n${msg}`}),
         })
         if (response.status == 204) {
             alert("Message Envoyé, merci 🙂")
@@ -28,6 +35,13 @@
 <div class="flex flex-col gap-2 w-full">
     <div class="flex gap-2">
         <Input type="text" placeholder="Nom" bind:value={nom}/>
+        <Input type="select" placeholder="Pôle" bind:value={pôle}>
+            <option value="<@&1226550229113770084>">Pôle Communication</option>
+            <option value="<@&1226550092786438224>">Bureau Restreint</option>
+            <option value="<@&1226550269073162403>">Pôle Tutorat</option>
+            <option value="<@&1226550404351922207>">Pôle Event</option>
+            <option value="@everyone">Autre</option>
+        </Input>
         <Input type="text" placeholder="Mail ou réseau social" bind:value={contact}/>
     </div>
     
