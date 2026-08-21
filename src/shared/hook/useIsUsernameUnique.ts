16981386c71f0ca9a84ca88f38a56ae43fdecdac
Victor <victor.dessaigne@proton.me>
2026-08-21 13:43:58 +0200

    Ajout d'un fichier non push

---
+++ src/shared/hook/useIsUsernameUnique.ts
@@ -0,0 +1,25 @@
+import pb from "@/shared/lib/pocketbase";
+import { useQuery } from "@tanstack/react-query";
+import type { ClientResponseError } from "pocketbase";
+
+
+
+
+
+export default function useIsUsernameUnique(input: string | undefined) {
+
+  const comboBoxQuery = useQuery<boolean, ClientResponseError>({
+    queryKey: ['user-search', input],
+    queryFn: async () => { 
+      const requestResult = await pb.collection('users').getList(1, 1, { filter: `username = "${input}"`, fields: 'id',})
+      if (!requestResult) throw Error("Error")
+      return requestResult.totalItems === 0;
+    },
+    enabled: !!input && input.length >= 2,
+    retry: false,
+  });
+
+
+  return {isLoading: comboBoxQuery.isLoading, isUnique: comboBoxQuery.data, error: comboBoxQuery.error}
+
+}
